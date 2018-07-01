558c04b9bbb685b4e088ddb22fc6ab4668e3388c
Michael Hamilton <michael.ts.hamilton@gmail.com>
2018-07-01 09:44:12 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,5 +1,9 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-def display_board
-  puts [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+def display_board(choice)
+  puts " #{choice[0]} | #{choice[1]} | #{choice[2]} "
+  puts "-----------"
+  puts " #{choice[3]}| #{choice[4]} | #{choice[5]} "
+  puts "-----------"
+  puts " #{choice[6]} | #{choice[7]} | #{choice[8]} "
 end
